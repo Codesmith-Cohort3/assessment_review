677dc2e101a5fc9bf0a746b89a36e965860d0bc8
Andrew Carlson <acarl005@g.ucla.edu>
2015-10-03 23:58:20 -0700

    make the assessment

---
+++ README.md
@@ -0,0 +1,12 @@
+# Precourse Assessment
+
+This will assess your learning of Javascript, HTML, and CSS from the precourse. There are no tests. It is up to you to determine if your answers are correct.
+
+### Part 1 - Javascript
+
+Complete the challenges in the `main.js`. To test your answers, you might want to test them out in the Chrome console or in the terminal with NodeJS.
+
+
+### Part 2 - HTML/CSS
+
+In the time available, create the closest possible clone of [this website](http://www.berkshirehathaway.com/). Do not use the Chrome DevTools to inspect the DOM. What will you use to achieve the two-column layout? Tables? Divs? Do this in `index.html` and `style.css`.
---
+++ main.js
@@ -0,0 +1,50 @@
+// Write a function that takes a string and capitalizes every vowel in it
+// capitalizeVowels('hella world');  -> 'hEllA wOrld'
+function capitalizeVowels(str) {
+
+}
+
+// Write a function that takes an array and two integers as arguments. These two numbers will be indeces. All array elements between (and including) these positions should be reversed.
+// var zeroToTen = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// reverseChunk(zeroToTen, 2, 6);  -> [0, 1, 6, 5, 4, 3, 2, 7, 8, 9, 10]
+// var primes = [2, 3, 5, 7, 11];
+// reverseChunk(primes, 1, 4);  -> [2, 11, 7, 5, 3]
+function reverseChunk(arr, start, end) {
+
+}
+
+
+// Write a function that takes a string and converts it from snake case to camel case.
+// snake case: words_separated_by_underscores
+// camel case: wordsSeparatedByCapitals
+// snakeToCamel("hello_there_world");  -> "helloThereWorld"
+function snakeToCamel(str) {
+
+}
+
+
+// Write a function that takes an object and returns a nested array. These inner arrays will each have two elements: the key-value pairs from the object. These whole array should be sorted in alphabetical order by the key names.
+// var phoneNums = {
+//   chris: 3429588375,
+//   andy: 4829574932,
+//   mildew: 9975723073
+// }
+// objToSortedArray(phoneNums);  -> [ ['andy', 4829574932], 
+//                                    ['chris', 3429588375], 
+//                                    ['mildew', 9975723073] ]
+function objToSortedArray(obj) {
+
+}
+
+
+// Write a function that returns a function. The returned function accepts one argument and returns an array of all previous arguments that have been passed to it.
+// var arrBuilder1 = arrayFactory();
+// arrBuilder1('whatever');  -> ['whatever']
+// arrBuilder1('we');        -> ['whatever', 'we']
+// arrBuilder1('want');      -> ['whatever', 'we', 'want']
+// var arrBuilder2 = arrayFactory();
+// arrBuilder2(1);           -> [1] 
+// arrBuilder2(['two']);     -> [1, ['two']] 
+function arrayFactory() {
+
+} 
