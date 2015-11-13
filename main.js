1fe14f5beecc052d842695f59bdc4754a9243f2e
Brian <bgrober@gmail.com>
2015-11-13 11:05:04 -0800

    done

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 // Write a function that takes a string and capitalizes every vowel in it
 // capitalizeVowels('hella world');  -> 'hEllA wOrld'
 function capitalizeVowels(str) {
-
+  str.replace(/[aeiou]/gi, function(letter){
+    return letter.toUpperCase();
+  });
+  return str;
 }
 
 // Write a function that takes an array and two integers as arguments. These two numbers will be indeces. All array elements between (and including) these positions should be reversed.
@@ -10,7 +13,10 @@ function capitalizeVowels(str) {
 // var primes = [2, 3, 5, 7, 11];
 // reverseChunk(primes, 1, 4);  -> [2, 11, 7, 5, 3]
 function reverseChunk(arr, start, end) {
-
+  var firstHalf = arr.slice(0, start);
+  var secondHalf = arr.slice(start, end + 1).reverse();
+  var thirdPart = arr.slice(end + 1);
+  return firstHalf.concat(secondHalf, thirdPart);
 }
 
 
@@ -19,7 +25,9 @@ function reverseChunk(arr, start, end) {
 // camel case: wordsSeparatedByCapitals
 // snakeToCamel("hello_there_world");  -> "helloThereWorld"
 function snakeToCamel(str) {
-
+  return str.replace(/(_\w)/g, function(letter) {
+    return letter[1].toUpperCase();
+  } );
 }
 
 
@@ -33,5 +41,9 @@ function snakeToCamel(str) {
 //                                    ['chris', 3429588375],
 //                                    ['mildew', 9975723073] ]
 function objToSortedArray(obj) {
-
+  var result = [];
+  for(var i in obj) {
+    result.push([i,obj[i]]);
+  }
+  return result.sort();
 }
